87726893cfd8b784ddd436fbc4c3a6d8d913eb06
nealder <nealder@freemail.hu>
2017-05-31 12:50:17 +0200

    Delete vialgeinsetn.tsx

--- src/main/resources/Maps/TileEditor/vialgeinsetn.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="c165721e46cfa65d654a80fb1605e15c" tilewidth="32" tileheight="32" tilecount="49" columns="7">
- <image source="c165721e46cfa65d654a80fb1605e15c.jpg" width="236" height="236"/>
-</tileset>
