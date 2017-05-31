a54339510296a947bba375bbee29ba8b1d39e42f
nealder <nealder@freemail.hu>
2017-05-31 12:48:52 +0200

    Delete dungeontileset.tsx

--- src/main/resources/Maps/TileEditor/dungeontileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="dungeontile" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="3b8336d89d0b69c066ed85861e0b887a.png" width="512" height="512"/>
-</tileset>
