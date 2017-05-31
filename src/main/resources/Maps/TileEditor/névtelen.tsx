45d45b88da4b281aba33c8d1810fca10288c901a
nealder <nealder@freemail.hu>
2017-05-31 12:49:54 +0200

    Delete névtelen.tsx

--- src/main/resources/Maps/TileEditor/névtelen.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="QuxSo" tilewidth="32" tileheight="32" tilecount="550" columns="22">
- <image source="QuxSo.png" width="704" height="800"/>
-</tileset>
